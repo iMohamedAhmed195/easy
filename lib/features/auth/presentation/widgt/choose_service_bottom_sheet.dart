5ad4f747776f521d45c309b5945b024d4f517972
axah710 <ahmed36085361@gmail.com>
2024-10-22 22:06:52 +0300

    Refactor UiAuthCubit and ChooseServiceBottomSheet for Enhanced Functionality and Code Clarity
    - Introduced a new `selectedServices` list in `UiAuthCubit` to manage multiple service type selections.
    - Implemented `toggleServiceType` method to toggle selection for each service type and ensure at least one service is always selected.
    - Added `isServiceSelected` method to check if a specific service type is selected.
    - Updated `ChooseServiceBottomSheet` to use the new `toggleServiceType` method for service selection.
    - Enhanced logging in `GlobalBlocObserver` for better debugging and monitoring of Bloc/Cubit lifecycle events, state changes, transitions, and errors.
    - Improved code readability and consistency by refactoring variable declarations, method signatures, and widget structures.
    
    This refactor enhances the user experience by allowing multiple service type selections and improves the maintainability of the codebase through clearer and more concise code.

--- lib/features/auth/presentation/widgt/choose_service_bottom_sheet.dart
+++ lib/features/auth/presentation/widgt/choose_service_bottom_sheet.dart
@@ -16,35 +16,27 @@ class ChooseServiceBottomSheet extends StatelessWidget {
           height: 324.h,
           child: Column(
             children: [
-              SizedBox(
-                height: 8.h,
-              ),
+              SizedBox(height: 8.h),
               Container(
                 height: 4.h,
                 width: 72.w,
                 decoration: BoxDecoration(
-                    color: AppColors.gray,
-                    borderRadius: BorderRadius.circular(78.r)),
+                  color: AppColors.gray,
+                  borderRadius: BorderRadius.circular(78.r),
+                ),
               ),
-              SizedBox(
-                height: 12.h,
-              ),
+              SizedBox(height: 12.h),
               Text(
                 AppStrings().chooseService,
-                style: getBoldTextStyle(
-                    fontSize: 16, color: AppColors.black),
+                style: getBoldTextStyle(fontSize: 16, color: AppColors.black),
               ),
-              SizedBox(
-                height: 16.h,
-              ),
+              SizedBox(height: 16.h),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   GestureDetector(
                     onTap: () {
-                      context
-                          .read<UiAuthCubit>()
-                          .changeServiceType();
+                      context.read<UiAuthCubit>().toggleServiceType('money');
                     },
                     child: Container(
                       width: 160.w,
@@ -52,30 +44,33 @@ class ChooseServiceBottomSheet extends StatelessWidget {
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10.r),
                         border: Border.all(
-                            color: context
-                                .read<UiAuthCubit>()
-                                .isMoneyProvider
-                                ? AppColors.primaryColor
-                                : AppColors.borderColor2,
-                            width: 1),
+                          color: context
+                                  .read<UiAuthCubit>()
+                                  .isServiceSelected('money')
+                              ? AppColors.primaryColor
+                              : AppColors.borderColor2,
+                          width: 1,
+                        ),
                       ),
                       child: Column(
                         children: [
                           SizedBox(
-                              width: 123.w,
-                              height: 123.h,
-                              child: SvgPicture.asset(AppAssets().money)),
+                            width: 123.w,
+                            height: 123.h,
+                            child: SvgPicture.asset(AppAssets().money),
+                          ),
                           SizedBox(height: 14.h),
                           Text(
                             AppStrings().money,
                             style: getSemiboldTextStyle(
-                                fontSize: 10,
-                                color: context
-                                    .read<UiAuthCubit>()
-                                    .isMoneyProvider
-                                    ? AppColors.primaryColor
-                                    : AppColors.borderColor2),
-                          )
+                              fontSize: 10,
+                              color: context
+                                      .read<UiAuthCubit>()
+                                      .isServiceSelected('money')
+                                  ? AppColors.primaryColor
+                                  : AppColors.borderColor2,
+                            ),
+                          ),
                         ],
                       ),
                     ),
@@ -83,9 +78,7 @@ class ChooseServiceBottomSheet extends StatelessWidget {
                   SizedBox(width: 8.w),
                   GestureDetector(
                     onTap: () {
-                      context
-                          .read<UiAuthCubit>()
-                          .changeServiceType();
+                      context.read<UiAuthCubit>().toggleServiceType('provider');
                     },
                     child: Container(
                       width: 160.w,
@@ -93,55 +86,63 @@ class ChooseServiceBottomSheet extends StatelessWidget {
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10.r),
                         border: Border.all(
-                            color: !context
-                                .read<UiAuthCubit>()
-                                .isMoneyProvider
-                                ? AppColors.primaryColor
-                                : AppColors.borderColor2,
-                            width: 1),
+                          color: context
+                                  .read<UiAuthCubit>()
+                                  .isServiceSelected('provider')
+                              ? AppColors.primaryColor
+                              : AppColors.borderColor2,
+                          width: 1.w,
+                        ),
                       ),
                       child: Column(
                         children: [
                           SizedBox(
-                              width: 123.w,
-                              height: 123.h,
-                              child: SvgPicture.asset(AppAssets().oil)),
+                            width: 123.w,
+                            height: 123.h,
+                            child: SvgPicture.asset(AppAssets().oil),
+                          ),
                           SizedBox(height: 14.h),
                           Text(
                             AppStrings().provider,
                             style: getSemiboldTextStyle(
-                                fontSize: 10,
-                                color: !context
-                                    .read<UiAuthCubit>()
-                                    .isMoneyProvider
-                                    ? AppColors.primaryColor
-                                    : AppColors.borderColor2),
-                          )
+                              fontSize: 10,
+                              color: context
+                                      .read<UiAuthCubit>()
+                                      .isServiceSelected('provider')
+                                  ? AppColors.primaryColor
+                                  : AppColors.borderColor2,
+                            ),
+                          ),
                         ],
                       ),
                     ),
-                  )
+                  ),
                 ],
               ),
-              SizedBox(
-                height: 32.h,
-              ),
+              SizedBox(height: 32.h),
               Padding(
-                padding:  EdgeInsets.symmetric(horizontal: 16.w),
+                padding: EdgeInsets.symmetric(horizontal: 16.w),
                 child: CustomButton(
-                    text: AppStrings().submit,
-                    onPressed: (){
+                  text: AppStrings().submit,
+                  onPressed: () {
+                    if (context
+                        .read<UiAuthCubit>()
+                        .selectedServices
+                        .isNotEmpty) {
                       Routes.awesomeSuccessRoute.moveToWithArgs({
                         "header": AppStrings().congratulations,
                         "subHeader": AppStrings().accountCreated,
                         "buttonText": AppStrings().proceed,
                         "onPressed": () {
-                           Routes.bottomNavigationRoute.moveTo();
-                        }
-                      },
-                      );
-                    }),
-              )
+                          Routes.bottomNavigationRoute.moveTo();
+                        },
+                      });
+                    } else {
+                      //! Here We May Show an error message or dialog to avoid empty selection ...
+                    }
+                  },
+                ),
+              ),
             ],
           ),
         );
