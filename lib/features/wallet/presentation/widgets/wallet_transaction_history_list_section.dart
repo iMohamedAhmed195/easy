86f5e71e87b8412c80cb970d20743c6aef7bf68f
axah710 <ahmed36085361@gmail.com>
2024-11-15 04:29:22 +0200

    Refactor wallet transaction history list section to use a reusable widget
    Previously, the wallet transaction history list section contained repetitive UI code for each transaction item. This commit introduces a new `WalletHistiryListItemSection` widget to encapsulate the UI for individual transaction items. The `WalletTransactionHistoryListSection` widget is updated to use this new reusable widget, reducing code duplication and improving maintainability.

--- lib/features/wallet/presentation/widgets/wallet_transaction_history_list_section.dart
+++ lib/features/wallet/presentation/widgets/wallet_transaction_history_list_section.dart
@@ -1,3 +1,5 @@
+import 'package:easy/features/wallet/presentation/widgets/wallet_histiry_list_item_section.dart';
+
 import '../../../../exports.dart';
 import 'transaction_model_data.dart';
 
@@ -12,92 +14,8 @@ class WalletTransactionHistoryListSection extends StatelessWidget {
         itemCount: transactions.length,
         itemBuilder: (context, index) {
           final transaction = transactions[index];
-          return Container(
-            padding: EdgeInsets.all(12.w),
-            margin: EdgeInsets.symmetric(vertical: 8.h),
-            decoration: ShapeDecoration(
-              color: AppColors.backGroundGray,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.r),
-              ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SvgDisplayer(
-                      assetName: transaction.iconPath,
-                      height: 40.h,
-                      width: 40.w,
-                    ),
-                    8.hs,
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                transaction.transactionType,
-                                style: getSemiBoldTextStyle(
-                                  fontSize: 12,
-                                  color: AppColors.richCharcoal,
-                                ),
-                              ),
-                              Container(
-                                padding: 8.ph + 2.pv,
-                                decoration: ShapeDecoration(
-                                  color: AppColors.brightGreenColor,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.r),
-                                  ),
-                                ),
-                                child: Text(
-                                  transaction.statusMessage,
-                                  style: getSemiBoldTextStyle(
-                                    fontSize: 10,
-                                    color: AppColors.strongGreen,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          4.vs,
-                          Row(
-                            children: [
-                              Text(
-                                '${transaction.isMoneyRaising ? '+' : '-'}${transaction.moneyAmount} SAR',
-                                style: getSemiBoldTextStyle(
-                                  fontSize: 20,
-                                  color: transaction.moneyColor,
-                                ),
-                              ),
-                              8.hs,
-                              SvgDisplayer(
-                                assetName: transaction.arrowIconPath,
-                                height: 18.h,
-                                width: 18.w,
-                              ),
-                            ],
-                          ),
-                          8.hs,
-                          Text(
-                            transaction.date,
-                            style: getRegularTextStyle(
-                              fontSize: 12,
-                              color: AppColors.grayishBlueColor,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+          return WalletHistiryListItemSection(
+            transaction: transaction,
           );
         },
       ),
