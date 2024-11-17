3a2323b4c2294c67ee1dfbddc4be5d8d154f41c3
axah710 <ahmed36085361@gmail.com>
2024-11-17 06:50:34 +0200

    Refactor ManageBankCardsTrailingIconMenuSection widget
    Extract `_buildPopupMenuItems` and `_buildMenuItemContent` methods to improve readability and maintainability. This refactor encapsulates the logic for building popup menu items and their content, making the main widget body cleaner and easier to understand.

--- lib/features/manage_cards/presentation/widgets/manage_bank_cards_trailing_icon_menu_section.dart
+++ lib/features/manage_cards/presentation/widgets/manage_bank_cards_trailing_icon_menu_section.dart
@@ -1,7 +1,5 @@
 import 'dart:developer';
-
 import 'package:flutter/foundation.dart';
-
 import '../../../../exports.dart';
 import '../../model/manage_cards_drop_down_menu_model.dart';
 import 'manage_cards_deatils_drop_down_menu_model_data.dart';
@@ -19,38 +17,44 @@ class ManageBankCardsTrailingIconMenuSection extends StatelessWidget {
         width: 24.w,
       ),
       onSelected: (DropdownItem selectedItem) {
-        //! Handle item selection
         if (kDebugMode) {
           log('Selected: ${selectedItem.value}');
         }
       },
       itemBuilder: (BuildContext context) {
-        return dropdownItems.map((DropdownItem item) {
-          int index = dropdownItems.indexOf(item); //! Get the index of the item
-          return PopupMenuItem<DropdownItem>(
-            value: item,
-            child: Row(
-              children: [
-                SvgDisplayer(
-                  assetName: item.icon,
-                  height: 16.h,
-                  width: 16.w,
-                  svgIconColor:
-                      index == 2 ? AppColors.red : AppColors.primaryColor,
-                ),
-                8.hs,
-                Text(
-                  item.name,
-                  style: getSemiBoldTextStyle(
-                    fontSize: 12.54,
-                    color: index == 2 ? AppColors.red : AppColors.richCharcoal,
-                  ),
-                ),
-              ],
-            ),
-          );
-        }).toList();
+        return _buildPopupMenuItems();
       },
+    );
+  }
+
+  List<PopupMenuItem<DropdownItem>> _buildPopupMenuItems() {
+    return dropdownItems.map((DropdownItem item) {
+      int index = dropdownItems.indexOf(item);
+      return PopupMenuItem<DropdownItem>(
+        value: item,
+        child: _buildMenuItemContent(item, index),
+      );
+    }).toList();
+  }
+
+  Widget _buildMenuItemContent(DropdownItem item, int index) {
+    return Row(
+      children: [
+        SvgDisplayer(
+          assetName: item.icon,
+          height: 16.h,
+          width: 16.w,
+          svgIconColor: index == 2 ? AppColors.red : AppColors.primaryColor,
+        ),
+        8.hs,
+        Text(
+          item.name,
+          style: getSemiBoldTextStyle(
+            fontSize: 12.54,
+            color: index == 2 ? AppColors.red : AppColors.richCharcoal,
+          ),
+        ),
+      ],
     );
   }
 }
